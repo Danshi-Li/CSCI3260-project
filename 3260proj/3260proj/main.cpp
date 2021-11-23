e8d9280de10fdd71c55027b1ca24819ee4232524
chiaoso <1155124591@link.cuhk.edu.hk>
2021-11-23 19:53:22 +0800

    .

---
+++ 3260proj/3260proj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3260proj
+//
+//  Created by goodgame on 2021/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
